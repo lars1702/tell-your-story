dbd915b441b6811023054ca0abc8fef57ff426aa
lars1702 <lars1702@gmail.com>
2019-03-21 22:10:14 +0100

    finished the spinner. Also ejected the Create React App.

--- client/src/components/pages/home/Home.js
+++ client/src/components/pages/home/Home.js
@@ -3,41 +3,51 @@ import {
   Header,
   BigText,
   TextSlider,
+  SliderBox,
 } from "./components"
 
 class Home extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      slider: []
+      sliderCounter: 0,
+      sliderTexts: [],
+      currentSliderText: "Click here to write you story."
     }
+    
   }
 
   componentDidMount() {
     this.setState({
-      slider: [
+      sliderTexts: [
         "Click here to write you story.",
         "Click here to get peace of mind.",
         "Click here to help stop sexual harrasment.",
       ]
     })
+    this.interval = setInterval(() => {
+      this.setState({
+        sliderCounter: Math.floor((this.state.sliderCounter+1) % 3),
+        currentSliderText: this.state.sliderTexts[this.state.sliderCounter]
+      })
+    }, 4000)
   }
+
   render() {
+    const { sliderCounter, currentSliderText } = this.state
+    console.log('DEBUG - sliderCounter', sliderCounter)
+    console.log('DEBUG - currentSliderText', currentSliderText)
     return (
       <div className="Home">
         <Header>
           <BigText>Inspiring tagline or quote about opening up</BigText>
         </Header>
         <Header>
-          <TextSlider>
-            {this.state.slider?.length &&
-              this.state.slider.map((e, i) => {
-                return setInterval(() => {
-                  return "This should show"
-                }, 3000)
-              })
-            }
-          </TextSlider>
+          <SliderBox>
+            <TextSlider>
+              {this.state.currentSliderText}
+            </TextSlider>
+          </SliderBox>
         </Header>
       </div>
     )
--- client/src/components/pages/home/components.js
+++ client/src/components/pages/home/components.js
@@ -1,4 +1,23 @@
-import styled from "styled-components"
+import styled, { keyframes } from "styled-components"
+
+const spinner = keyframes`
+  0% {
+    transform: translateY(100px);
+    opacity: 0;
+  }
+  20% {
+    transform: translateY(0px);
+    opacity: 1;
+  }
+  80% {
+    transform: translateY(0px);
+    opacity: 1;
+  }
+  100% {
+    transform: translateY(-100px);
+    opacity: 0;
+  }
+`
 
 export const Header = styled.div`
   background-image: url("/Berliiin.jpg");
@@ -37,6 +56,16 @@ export const BigText = styled.h2`
 export const TextSlider = styled.h2`
   font-family: 'Fira Sans', sans-serif;
   font-size: 44px;
-  height: min-content;
+  animation: ${spinner} 4s cubic-bezier(0.550, 0.055, 0.675, 0.190) 4s infinite both;
+`
+
+export const SliderBox = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid red;
   z-index: 1;
+  height: 150px;
+  overflow-y: hidden;
+  width: 100%;
 `
