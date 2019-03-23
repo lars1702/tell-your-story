94161ed33bc1a3eec9976cc2eb66cff61c7ff11d
lars1702 <lars1702@gmail.com>
2019-03-23 23:17:16 +0100

    added feed example!

---
+++ client/src/components/pages/Feed.js
@@ -0,0 +1,99 @@
+import React from "react"
+
+class Feed extends React.Component {
+  render() {
+    return (
+      <li class="js-stream-item stream-item stream-item" data-item-id="1109423642583154691" id="stream-item-tweet-1109423642583154691" data-item-type="tweet" data-suggestion-json="{&quot;suggestion_details&quot;:{&quot;suggestion_type&quot;:&quot;RankedOrganicTweet&quot;,&quot;controller_data&quot;:&quot;DAABCgABAAEMQgECAEEAAA==&quot;},&quot;tweet_ids&quot;:&quot;1109423642583154691&quot;,&quot;scribe_component&quot;:&quot;tweet&quot;}">
+        <div class="tweet js-stream-tweet js-actionable-tweet js-profile-popup-actionable dismissible-content original-tweet js-original-tweet has-cards dismissible-content cards-forward" data-tweet-id="1109423642583154691" data-item-id="1109423642583154691" data-permalink-path="/stephenfry/status/1109423642583154691" data-conversation-id="1109423642583154691" data-tweet-nonce="1109423642583154691-c45d1567-c721-45fc-a61a-e765c43fc0ca" data-screen-name="stephenfry" data-name="Stephen Fry" data-user-id="15439395" data-you-follow="true" data-follows-you="false" data-you-block="false" data-reply-to-users-json="[{&quot;id_str&quot;:&quot;15439395&quot;,&quot;screen_name&quot;:&quot;stephenfry&quot;,&quot;name&quot;:&quot;Stephen Fry&quot;,&quot;emojified_name&quot;:{&quot;text&quot;:&quot;Stephen Fry&quot;,&quot;emojified_text_as_html&quot;:&quot;Stephen Fry&quot;}}]" data-disclosure-type="" data-card2-type="summary_large_image" data-has-cards="true" data-component-context="suggest_ranked_organic_tweet" data-tweet-stat-initialized="true">
+          <div class="context">
+          </div>
+          <div class="content">
+            <div class="js-tweet-text-container"><h2>Office party harassment</h2>
+              <p class="TweetTextSize  js-tweet-text tweet-text" lang="en" data-aria-label-part="0">I was once at an office party, and one of my coworkers kept taking photos of by boobs. When i told him to stop he just smiled, and when i told my boss he said i just just expect that kind of behaviour.<a href="https://t.co/XsNorT87tP" rel="nofollow noopener" dir="ltr" data-expanded-url="https://news.sky.com/story/uri-geller-tells-pm-i-am-going-to-stop-brexit-telepathically-11672961" class="twitter-timeline-link u-hidden" title="https://news.sky.com/story/uri-geller-tells-pm-i-am-going-to-stop-brexit-telepathically-11672961"><span class="tco-ellipsis"></span><span class="invisible">https://</span><span class="js-display-url">news.sky.com/story/uri-gell</span><span class="invisible">er-tells-pm-i-am-going-to-stop-brexit-telepathically-11672961</span><span class="tco-ellipsis"><span class="invisible">&nbsp;</span>…</span></a></p>
+            </div>
+            <div class="stream-item-footer">
+              <div class="ProfileTweet-actionCountList u-hiddenVisually">
+                <span class="ProfileTweet-action--reply u-hiddenVisually">
+                  <span class="ProfileTweet-actionCount" data-tweet-stat-count="296">
+                    <span class="ProfileTweet-actionCountForAria" id="profile-tweet-action-reply-count-aria-1109423642583154691" data-aria-label-part="">296 replies</span>
+                  </span>
+                </span>
+                <span class="ProfileTweet-action--retweet u-hiddenVisually">
+                  <span class="ProfileTweet-actionCount" data-tweet-stat-count="839">
+                    <span class="ProfileTweet-actionCountForAria" id="profile-tweet-action-retweet-count-aria-1109423642583154691" data-aria-label-part="">839 retweets</span>
+                  </span>
+                </span>
+                <span class="ProfileTweet-action--favorite u-hiddenVisually">
+                  <span class="ProfileTweet-actionCount" data-tweet-stat-count="5533">
+                    <span class="ProfileTweet-actionCountForAria" id="profile-tweet-action-favorite-count-aria-1109423642583154691" data-aria-label-part="">5,533 likes</span>
+                  </span>
+                </span>
+              </div>
+              <div class="ProfileTweet-actionList js-actions" role="group" aria-label="Tweet actions">
+                <div class="ProfileTweet-action ProfileTweet-action--reply">
+                  <button class="ProfileTweet-actionButton js-actionButton js-actionReply" data-modal="ProfileTweet-reply" type="button" aria-describedby="profile-tweet-action-reply-count-aria-1109423642583154691">
+                    <div class="IconContainer js-tooltip" data-original-title="Reply">
+                      <span style={{fontSize: "11px"}}>Me too</span>
+                      <span class="u-hiddenVisually">Reply</span>
+                    </div>
+                    <span class="ProfileTweet-actionCount" data-tweet-stat-count="296" style={{marginLeft: "5px"}}>
+                      <span class="ProfileTweet-actionCountForPresentation" aria-hidden="true">296</span>
+                    </span>
+                  </button>
+                </div>
+
+                <div class="ProfileTweet-action ProfileTweet-action--favorite js-toggleState">
+                  <button class="ProfileTweet-actionButton js-actionButton js-actionFavorite" type="button" aria-describedby="profile-tweet-action-favorite-count-aria-1109423642583154691">
+                    <div class="IconContainer js-tooltip" data-original-title="Like">
+                      <span role="presentation" class="Icon Icon--heart Icon--medium"></span>
+                      <div class="HeartAnimation"></div>
+                      <span class="u-hiddenVisually">Like</span>
+                    </div>
+                    <span class="ProfileTweet-actionCount" data-tweet-stat-count="5533">
+                      <span class="ProfileTweet-actionCountForPresentation" aria-hidden="true">5.5K</span>
+                    </span>
+
+                  </button><button class="ProfileTweet-actionButtonUndo ProfileTweet-action--unfavorite u-linkClean js-actionButton js-actionFavorite" type="button">
+                    <div class="IconContainer js-tooltip" title="Undo like">
+                      <span role="presentation" class="Icon Icon--heart Icon--medium"></span>
+                      <div class="HeartAnimation"></div>
+                      <span class="u-hiddenVisually">Liked</span>
+                    </div>
+                    <span class="ProfileTweet-actionCount" data-tweet-stat-count="5533">
+                      <span class="ProfileTweet-actionCountForPresentation" aria-hidden="true">5.5K</span>
+                    </span>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="dismiss-module">
+          <div class="dismissed-module">
+            <div class="feedback-actions">
+              <div class="feedback-action" data-feedback-type="DontLike" data-feedback-url="/2/timeline/feedback.json?feedback_type=DontLike&amp;action_metadata=FWA5DAA%3D">
+                <div class="action-confirmation dismiss-module-item">Thanks. Twitter will use this to make your timeline better.
+            <span class="undo-action">Undo</span>
+                </div>
+                <div class="child-feedback-prompt dismiss-module-item" data-child-feedback-confirmation="Thanks. You’ll see fewer Tweets from Stephen Fry." data-child-feedback-url="/2/timeline/feedback.json?feedback_type=SeeFewer&amp;action_metadata=FWAVAikcJsbY3A4AFoCg9qcTAA%3D%3D">
+                  Show fewer Tweets from Stephen Fry
+            </div>
+                <div class="child-feedback-prompt dismiss-module-item" data-child-feedback-confirmation="Thanks. You’ll see fewer Tweets like this." data-child-feedback-url="/2/timeline/feedback.json?feedback_type=SeeFewer&amp;action_metadata=FWA5HBaGgLaZmbu75R4AFoCg9qcTAA%3D%3D">
+                  This Tweet isn’t relevant
+            </div>
+              </div>
+            </div>
+            <div class="child-feedback-confirmation">
+              <div class="child-confirmation-item">
+                <span class="child-confirmation-text"></span>
+                <span class="undo-child-feedback-action">Undo</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </li>
+    )
+  }
+}
+
+export default Feed
